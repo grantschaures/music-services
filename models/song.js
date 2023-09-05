5a1551e7287ed0bbf15782a0c2b0e9b02fd3cddd
grantschaures <grantschaures32@gmail.com>
2023-09-05 09:18:21 -0700

    minor changes

--- models/song.js
+++ models/song.js
@@ -8,13 +8,13 @@
 
 const db = require("../db");
 
-// Create a model from the schema
+// Create a Song model from the schema
 const Song = db.model("Song", {
-   title:       { type: String, required: true },
-   artist:      String,
-   popularity:  { type: Number, min: 1, max: 10 },
-   releaseDate: { type: Date, default: Date.now },
-   genre:       [ String ]
+  title:       { type: String, required: true },
+  artist:      String,
+  popularity:  { type: Number, min: 1, max: 10 },
+  releaseDate: { type: Date, default: Date.now },
+  genre:       [ String ]
 });
 
 module.exports = Song;
